e58519d241470e303556f6409f008f339e2dc40e
Tomer <cnaan.tomer@gmail.com>
2020-04-18 21:04:37 +0300

    fixed require

--- routes/columnTypes.js
+++ routes/columnTypes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
+
 const { Column_type } = require("../models/columnType");
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
 	const columnTypes = await Column_type.find()
 		.sort("type")
 		.select("type options");
