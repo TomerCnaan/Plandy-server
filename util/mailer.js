30e12681e045ced1abe48a9ef3fe2b9dcd3064e9
Tomer <cnaan.tomer@gmail.com>
2020-05-05 11:05:48 +0300

    sending mails with smtp transporter

--- util/mailer.js
+++ util/mailer.js
@@ -8,21 +8,39 @@ function sendMail(emailReceiver, company, link) {
 	`;
 
 	let transporter = nodemailer.createTransport({
-		service: "gmail",
+		// service: "gmail",
+		host: "in-v3.mailjet.com",
+		port: 587,
+		secure: false,
+		// auth: {
+		// 	user: config.get("emailUser"),
+		// 	pass: config.get("emailPassword")
+		// }
+
+		// TODO: store in env vars
 		auth: {
-			user: config.get("emailUser"),
-			pass: config.get("emailPassword")
-		}
+			user: "bac0b40eddf516fa3c886b61661de614",
+			pass: "f09fbaa5aad6b4d9972eb24ec86d872a",
+		},
 	});
 
 	let mailOptions = {
 		from: `Plandy <${config.get("emailUser")}>`,
 		to: emailReceiver,
 		subject: `Invitation to join the ${company} account on Plandy🥳`,
-		html: emailbody
+		html: emailbody,
 	};
 
+	transporter.verify(function (error, success) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Server is ready to take our messages");
+		}
+	});
+
 	transporter.sendMail(mailOptions, (err, data) => {
+		console.log(data);
 		if (err) {
 			return winston.error(err);
 		}
